c6b4fbf366f9e160b45415496b0fe282c285cc3c
liannemoreno <lianne.moreno3@gmail.com>
2023-05-13 16:38:49 -0400

    initializing collections

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
 import './App.scss';
+import {React, useState, useEffect} from "react";
 import GetStarted from './pages/GetStarted/GetStarted.js';
-import Footer from './components /Footer.js'
+import Collections from './pages/Collections/Collections.js';
+import Footer from './components /Footer/Footer.js'
+import collectionsData from './data/collections.json';
+import brewingData from './data/brewing.json';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
+  const [collections, setCollection]=useState(collectionsData[0]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch('collections.json');
+      const json = await response.json();
+      setData(json);
+    }
+    fetchData();
+  }, []);
+
 
   return (
     <div className="App">
       <BrowserRouter>
-        {/* <Routes>
+        <Routes>
           <Route path='/' element={<GetStarted />}></Route>
           <Route path= '*' element= {<GetStarted />}></Route>
-        </Routes> */}
-        <Footer/>
+          <Route path= '/collections' element= {<Collections collections={collections} />}></Route>
+          {/* <Route path= '/collections/idCollect' element= {<Collections />}></Route> */}
+          {/* <Route path= '/prepare' element= {<Prepare />}></Route> */}
+          {/* <Route path= '/prepare/:idPrep' element= {<Prepare />}></Route> */}
+        </Routes>
+        {/* <Footer/> */}
       </BrowserRouter>
     </div>
   );
--- src/pages/Collections/Collections.js
+++ src/pages/Collections/Collections.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import collections from '../../data/collections.json';
+import {useState} from "react";
 
 const Collections = () => {
-  return (
-    <div>Collections</div>
-  )
+const [selectedCollection, setSelectedCollection] = useState(collections[0])
+ return (
+        <section className="collections">
+            {/* <img className="nav__item-image" src={video.image} alt="up next video" onClick={() => clickHandler(video.id)}/> */}
+        </section>
+    );
 }
+
 
 export default Collections
