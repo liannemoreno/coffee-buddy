554df7b1530478fd9d56374c8fed027fcc2aaadc
liannemoreno <lianne.moreno3@gmail.com>
2023-05-16 14:14:46 -0400

    styled collections

--- src/pages/Collections/Collections.js
+++ src/pages/Collections/Collections.js
@@ -1,42 +1,56 @@
 
 import React from 'react';
-import { useNavigate } from 'react-router-dom'; // Import navigate instead of useNavigate
+import '../Collections/Collections.scss'
+import { useNavigate } from 'react-router-dom'; 
 import Footer from '../../components/Footer/Footer.js';
 import pages from '../../assets/logos/nav-bar/pages.svg';
+import Header from '../../components/Header/Header.js';
 
 function Collections({ collections }) {
   const navigate = useNavigate();
   const collectionClick = (collectionId) => {
-    navigate(`/collections/${collectionId}`); 
+    navigate(`/collections/${collectionId}`);
   };
 
   return (
     <>
-      <section className="collections_page">
-        <h1 className="collections__title">Collections</h1>
-        <div className="collections__sort-by">
-          <p className="collections__sorty-by--title">Sort By</p>
-          <p className="collections__sorty-by--name">Name</p>
+    <Header />
+      <section className="collections">
+        <div className="collections__wrapper">
+          <h2 className="collections__title">Your Collections</h2>
+          <div className="collections__sort-by">
+            <p className="collections__sort-by--title">Sort By</p>
+            <p className="collections__sort-by--name">Name</p>
+            <p className="collections__sort-by--date">Date</p>
+
+          </div>
         </div>
-        <div className="collections">
-          {collections.map((collection) => (
-            <div className="collection" key={collection.id}>
-              <h2
-                className="collection_name"
-                onClick={() => collectionClick(collection.id)}
-              >
-                {collection.collection_name}
-              </h2>
-              <p className="collection__desc">{collection.collection_description}</p>
-              <p className="collection__user">By {collection.user}</p>
-              <p className="collection__time">
-                {new Date().toLocaleString('en-US', {dateStyle: 'short'})}
-              </p>
+        {collections.map((collection) => (
+          <div className="collection" key={collection.id}>
+    
+              <div className="collection__left">
+                <img className="collection__left--img" src={collection.image} alt="#"></img>
+              </div>
+              <div className="collection__right">
+                <h1
+                  className="collection__name"
+                  onClick={() => collectionClick(collection.id)}
+                >
+                  {collection.collection_name}
+                </h1>
+                <div className="collection__middle">
+                  <p className="collection__middle-desc">{collection.collection_description}</p>
+                  <p className="collection__middle-user">by {collection.user}</p>
+                </div>
+                <p className="collection__time">
+                  Created On {new Date().toLocaleString('en-US', { dateStyle: 'short' })}
+                </p>
+              </div>
             </div>
-          ))}
-        </div>
+          
+        ))}
         <div className="collections__pages">
-          <img src={pages} alt="pages image 123" />
+          <img className="collections__pages--img" src={pages} alt="pages image 123" />
         </div>
         <div className="collections__new">
           <p className="collections__new--header">+Create New Collection</p>
